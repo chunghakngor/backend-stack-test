c5327e55e1fad0433dae13b42fd9ff18000b180b
Chung Hak Ngor <chunghakngor@gmail.com>
2021-01-27 00:30:27 +1300

    init mongodb

--- build/index.js
+++ build/index.js
@@ -6,18 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var elasticsearch_1 = require("@elastic/elasticsearch");
+var pg_1 = require("pg");
 var config_1 = __importDefault(require("./util/config"));
 var logger_1 = __importDefault(require("./util/logger"));
-var testRoutes_1 = __importDefault(require("./routes/testRoutes"));
+var userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+var listingRoutes_1 = __importDefault(require("./routes/listingRoutes"));
 var app = express_1.default();
 var client = new elasticsearch_1.Client({ node: config_1.default.es.ELASTIC_SEARCH });
+var pool = new pg_1.Pool(config_1.default.db.POSTGRES_DB);
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(body_parser_1.default.json());
 /** Logger */
 app.use(function (req, res, next) {
-    logger_1.default("SERVER", "METHOD: [" + req.method + "] - URL: [" + req.url + "] - IP: [" + req.socket.remoteAddress + "]");
+    logger_1.default("SERVER", "METHOD: [" + req.method + "] - URL: [" + req.originalUrl + "] - IP: [" + req.socket.remoteAddress + "]");
     res.on("finish", function () {
-        logger_1.default("SERVER", "METHOD: [" + req.method + "] - URL: [" + req.url + "] - STATUS: [" + res.statusCode + "] - IP: [" + req.socket.remoteAddress + "]");
+        logger_1.default("SERVER", "METHOD: [" + req.method + "] - URL: [" + req.originalUrl + "] - STATUS: [" + res.statusCode + "] - IP: [" + req.socket.remoteAddress + "]");
     });
     next();
 });
@@ -26,7 +29,8 @@ app.get("/", function (req, res) {
     res.status(200).json({ status: "live" });
 });
 /** Routes */
-app.use(testRoutes_1.default);
+app.use("/api", userRoutes_1.default);
+app.use("/api", listingRoutes_1.default);
 /** 404 Routes */
 app.use(function (req, res) {
     res.status(404);
--- src/util/config.ts
+++ src/util/config.ts
@@ -4,6 +4,12 @@ dotenv.config();
 const SERVER_HOSTNAME: string = process.env.SERVER_HOSTNAME || "localhost";
 const SERVER_PORT: number = Number(String(process.env.SERVER_PORT)) || 4000;
 
+const MONGODB_HOST: string = process.env.MONGODB_HOST || "localhost";
+const MONGODB_PORT: number = Number(String(process.env.MONGODB_PORT)) || 27017;
+const MONGODB_USERNAME: string = process.env.MONGODB_USERNAME || "root";
+const MONGODB_PASSWORD: string = process.env.MONGODB_PASSWORD || "password";
+const MONGODB_DATABASE: string = process.env.MONGODB_DATABASE || "db";
+
 const POSTGRES_DB_HOST: string = process.env.POSTGRES_DB_HOST || "localhost";
 const POSTGRES_DB_PORT: number = Number(String(process.env.POSTGRES_DB_PORT)) || 5432;
 const POSTGRES_DB_DATABASE: string = process.env.POSTGRES_DB_DATABASE || "postgres";
@@ -26,6 +32,13 @@ export default {
       database: POSTGRES_DB_DATABASE,
       user: POSTGRES_DB_USER,
       password: PROGRESS_DB_PASSWORD,
+    },
+    MONGODB: {
+      host: MONGODB_HOST,
+      port: MONGODB_PORT,
+      username: MONGODB_USERNAME,
+      password: MONGODB_PASSWORD,
+      database: MONGODB_DATABASE,
     },
   },
   es: {
