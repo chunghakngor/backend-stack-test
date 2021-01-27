bde4c90ce7fa557b5a7fc106044d3deae35f4082
Chung Hak Ngor <chunghakngor@gmail.com>
2021-01-27 16:29:13 +1300

    remove build folder

--- build/util/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getTimeString = function () {
-    return new Date().toISOString();
-};
-var logger = function (NAMESPACE, log, options) {
-    options ? console.log("[" + getTimeString() + "] " + NAMESPACE + " - [INFO] - " + log, options) : console.log("[" + getTimeString() + "] " + NAMESPACE + " - [INFO] - " + log);
-};
-exports.default = logger;
